3cedf4612a13a3351824d696a6e9053aad7353f8
Ashish Bhandari <ashishbhandari642005@gmail.com>
2026-06-18 17:20:11 +0530

    ngins congiruration testing

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello my name is ashish bhandari and iam from HP");
+  res.send("Hello my name is ashish bhandari and iam from HP and learnig devops");
 });
 
 app.listen(3000);
